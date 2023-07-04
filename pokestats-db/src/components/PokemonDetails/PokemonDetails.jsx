241f3974e80e4c83f8441d40efe3f87e74cdf294
Mohammad Ali Zahir <m.alizahir786@gmail.com>
2023-07-04 04:12:32 -0400

    Added conditional styling to the buttons

--- pokestats-db/src/components/PokemonDetails/PokemonDetails.jsx
+++ pokestats-db/src/components/PokemonDetails/PokemonDetails.jsx
@@ -4,6 +4,27 @@ import "./PokemonDetails.css";
 
 const P = new Pokedex();
 
+const TYPES = [
+  { name: "normal", color: "#FFE4C4", textColor: "#000000" },
+  { name: "fire", color: "#FF6347", textColor: "#FFFFFF" },
+  { name: "water", color: "#00FFFF", textColor: "#FFFFF" },
+  { name: "grass", color: "#808000", textColor: "#FFFFF" },
+  { name: "electric", color: "#FFFF00", textColor: "#000000" },
+  { name: "ice", color: "#87CEEB", textColor: "#000000" },
+  { name: "fighting", color: "#8B000", textColor: "#000000" },
+  { name: "poison", color: "#191970", textColor: "#000000" },
+  { name: "ground", color: "#F0E68C", textColor: "#FFFFFF" },
+  { name: "flying", color: "#AFEEEE", textColor: "#000000" },
+  { name: "psychic", color: "#FF00FF", textColor: "#FFFFFF" },
+  { name: "bug", color: "#00FF00", textColor: "#FFFFFF" },
+  { name: "rock", color: "#F08080", textColor: "#FFFFFF" },
+  { name: "ghost", color: "#663399", textColor: "#FFFFFF" },
+  { name: "dark", color: "#000000", textColor: "#FFFFFF" },
+  { name: "dragon", color: "#00008B", textColor: "#FFFFFF" },
+  { name: "steel", color: "#7FFFD4", textColor: "#000000" },
+  { name: "fairy", color: "#48D1CC", textColor: "#FFFFFF" },
+];
+
 function PokemonDetails() {
   const [pokemonName, setPokemonName] = useState("");
   const [pokemonData, setPokemonData] = useState(null);
@@ -22,6 +43,11 @@ function PokemonDetails() {
         setError("This Pokemon does not exist");
         console.log("Error:", error);
       });
+  };
+
+  const getButtonType = (typeName) => {
+    const type = TYPES.find((type) => type.name === typeName.toLowerCase());
+    return type ? { backgroundColor: type.color, color: type.textColor } : {};
   };
 
   return (
@@ -44,7 +70,12 @@ function PokemonDetails() {
             <ul>
               {pokemonData.types.map((type) => (
                 <li key={type.type.name}>
-                  <button className="type-button">{type.type.name}</button>
+                  <button
+                    className="type-button"
+                    style={getButtonType(type.type.name)}
+                  >
+                    {type.type.name}
+                  </button>
                 </li>
               ))}
             </ul>
