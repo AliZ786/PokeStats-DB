980db0e2e9b06225e23275c1495f0115249e0e6b
Mohammad Ali Zahir <m.alizahir786@gmail.com>
2023-07-04 03:34:45 -0400

    Added type and styling

--- pokestats-db/src/components/PokemonDetails/PokemonDetails.jsx
+++ pokestats-db/src/components/PokemonDetails/PokemonDetails.jsx
@@ -1,30 +1,57 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Pokedex from "pokedex-promise-v2";
+import "./PokemonDetails.css";
 
 const P = new Pokedex();
 
 function PokemonDetails() {
+  const [pokemonName, setPokemonName] = useState("");
   const [pokemonData, setPokemonData] = useState(null);
+  const [error, setError] = useState("");
 
-  useEffect(() => {
+  const handleSearch = () => {
+    // Reset error message
+    setError("");
+
     // Make API request and update state
-    P.getPokemonByName("pikachu")
+    P.getPokemonByName(pokemonName.toLowerCase())
       .then((response) => {
         setPokemonData(response);
       })
       .catch((error) => {
+        setError("This Pokemon does not exist");
         console.log("Error:", error);
       });
-  }, []);
+  };
 
   return (
     <div>
-      {/* Render Pokemon details */}
-      {pokemonData && (
+      <div>
+        <input
+          type="text"
+          placeholder="Enter Pokemon name"
+          value={pokemonName}
+          onChange={(e) => setPokemonName(e.target.value)}
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
+      {/* Render Pokemon details or error message */}
+      {pokemonData ? (
         <div>
           <h2>{pokemonData.name}</h2>
-          {/* Display other Pokemon details */}
+          <div>
+            <h3>Type(s):</h3>
+            <ul>
+              {pokemonData.types.map((type) => (
+                <li key={type.type.name}>
+                  <button className="type-button">{type.type.name}</button>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
+      ) : (
+        error && <h3>{error}</h3>
       )}
     </div>
   );
