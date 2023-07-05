0c4d6596ac63dba6177ea8011bd7ae8ff5306a9f
AliZ786 <59709752+AliZ786@users.noreply.github.com>
2023-07-05 19:08:32 -0400

    Refactored some of the value

--- pokestats-db/src/App.js
+++ pokestats-db/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import PokemonDetails from "./components/PokemonDetails/PokemonDetails.jsx";
+import PokemonSearch from "./components/PokemonSearch/PokemonSearch.jsx";
 import TypesDisplay from "./components/TypesDisplay/TypesDisplay.jsx";
 
 function App() {
   return (
     <>
       <h1>Pokémon Details</h1>
-      <PokemonDetails />
+      <PokemonSearch />
       <main className="main">
         <TypesDisplay />
       </main>
